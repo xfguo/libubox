be416140606328d677283247553c122314e8ce8f
Felix Fietkau <nbd@openwrt.org>
2011-02-06 16:20:57 +0100

    add missing copyright headers

--- blobmsg_json.h
+++ blobmsg_json.h
@@ -1,3 +1,18 @@
+/*
+ * blobmsg - library for generating/parsing structured blob messages
+ *
+ * Copyright (C) 2010 Felix Fietkau <nbd@openwrt.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 2.1
+ * as published by the Free Software Foundation
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
 #ifndef __BLOBMSG_JSON_H
 #define __BLOBMSG_JSON_H
 
