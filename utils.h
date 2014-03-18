4f44401ae8d23465261cef80b87630ffccd5a864
Felix Fietkau <nbd@openwrt.org>
2014-03-18 14:02:40 +0100

    utils: add __constructor and __hidden defines
    Signed-off-by: Felix Fietkau <nbd@openwrt.org>

--- utils.h
+++ utils.h
@@ -155,6 +155,14 @@ static inline uint16_t __u_bswap16(uint16_t val)
 #define __packed __attribute__((packed))
 #endif
 
+#ifndef __constructor
+#define __constructor __attribute__((constructor))
+#endif
+
+#ifndef __hidden
+#define __hidden __attribute__((visibility("hidden")))
+#endif
+
 #ifndef BITS_PER_LONG
 #define BITS_PER_LONG (8 * sizeof(unsigned long))
 #endif
